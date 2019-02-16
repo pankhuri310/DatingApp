78b08fd10f5048d25985e2375305997a0bf195f4
pankhuri.neema <pankhuri.neema@omni-bridge.net>
2019-02-16 14:56:30 +0530

    End of section 10

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -25,6 +25,8 @@ namespace DatingApp.API.Helpers
                 opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
             });
             CreateMap<Photo, PhotosForDetailsDto>();
+            CreateMap<UserForUpdateDto, User>();
+
         }
     }
 }
